23af0805b6530b2dc8169584ebd4cca26fb4b461
Ruchi Saha <ruchi@Ruchi.localdomain>
2019-09-27 13:07:16 +0530

    execution gui

---
+++ Simulator/Execution.py
@@ -0,0 +1,66 @@
+
+
+
+
+import tkinter as tk
+
+
+OP_CODES={ 'COMP':'0000', 'MOV':'0001', 'INC':'0010',
+           'DEC':'0011', 'SET':'0100', 'HALT':'0101',
+           'JUMP':'0110','FETCH':'0111','CALL':'1000',
+           'JUMPEQ':'1001', 'ADD':'1010', 'MOD':'1011',
+           'OR':'1100', 'CHECK':'1101', 'AND':'1110'
+           }
+
+
+# OPERATIONS = {}
+
+REGISTERS = {'A':'0000','B':'0001','PC':'0010','SOC_FRONT':'0011',
+             'PROG_STACK_TOP':'0100','MAR':'0101','SOC_BACK':'0110',
+             'ITYPE':'0111', 'COMM':'1000', 'CTRL':'1001','MDR':'1010',
+             'ANS':'1011', 'IR': '1100', 'IHAR':'1101' }
+
+UNARY_OPS = ['INC', 'JUMP','FETCH','JUMPEQ', 'DEC']
+
+
+# def execute(file):
+#     for i in file:
+
+
+
+
+
+class ExecutionGUI(tk.Frame):
+    def __init__(self, root):
+
+        tk.Frame.__init__(self, root)
+        self.canvas = tk.Canvas(root, borderwidth=0, background="#ffffff")
+        self.frame = tk.Frame(self.canvas, background="#ffffff")
+        self.vsb = tk.Scrollbar(root, orient="vertical", command=self.canvas.yview)
+        self.canvas.configure(yscrollcommand=self.vsb.set)
+
+        self.vsb.pack(side="right", fill="y")
+        self.canvas.pack(side="left", fill="both", expand=True)
+        self.canvas.create_window((4,4), window=self.frame, anchor="nw",
+                                  tags="self.frame")
+
+        self.frame.bind("<Configure>", self.onFrameConfigure)
+
+        self.populate()
+
+    def populate(self):
+        '''Put in some fake data'''
+        for row in range(100):
+            tk.Label(self.frame, text="%s" % row, width=3, borderwidth="1",
+                     relief="solid").grid(row=row, column=0)
+            t="this is the second column for row %s" %row
+            tk.Label(self.frame, text=t).grid(row=row, column=1)
+
+    def onFrameConfigure(self, event):
+        '''Reset the scroll region to encompass the inner frame'''
+        self.canvas.configure(scrollregion=self.canvas.bbox("all"))
+
+if __name__ == "__main__":
+    root=tk.Tk()
+    Example(root).pack(side="top", fill="both", expand=True)
+    root.mainloop()
--- Simulator/main_gui.py
+++ Simulator/main_gui.py
@@ -10,12 +10,21 @@ from tkinter import *
 from tkinter import filedialog, messagebox
 from tkinter import ttk
 from customNotebook import CustomNotebook  #user defined
+import gui_assembler as assembler
+from Execution import *
 
 COLORS = {'color' : '#40E0D0'}
 ASSEMBLING = 0
 FILE_OPEN = 0
 EDITING = 0
 debug =0
+
+
+
+
+
+
+
 
 class ScrolledText(Frame):
     def __init__(self, parent, *args, **kwargs):
@@ -24,7 +33,7 @@ class ScrolledText(Frame):
         self.vsb = Scrollbar(self, orient="vertical", command=self.text.yview)
         self.text.configure(yscrollcommand=self.vsb.set)
         self.vsb.pack(side="right", fill="y")
-        self.text.pack(side="left", fill="both", expand=True)
+        self.text.pack(side="left", fill="both")
 
 
         # expose some text methods as methods on this object
@@ -42,32 +51,66 @@ class ScrolledText(Frame):
 
 
 class Code(Frame):
-    def __init__(self, parent):
-        Frame.__init__(self, parent, bg = 'gray89')
+    def __init__(self, parent,*args, **kwargs):
+        Frame.__init__(self, parent, *args, **kwargs)
         self.scrolled_text = None
         self.current_file = None
+        self.cn = CustomNotebook(self, width=920, height= 500)
+        self.cn.pack(side="top", fill="both")
+        self.tabs = dict()
+        self.filecount=0
+
 
     def onOpen(self):
-        global FILE_OPEN
+
+
         global root
         global runmenu
 
 
-        ftypes = [('Assembly files', '*.asm'), ('All files', '*')]
+        ftypes = [('Assembly files', '*.asm')]
         dlg = filedialog.Open(self, filetypes = ftypes)
         filename = dlg.show()
         self.current_file = filename
-        # print("file:  ", filename, type(filename))
+
+
+        to_be_deleted = list()
+        for i in self.tabs.keys():
+            if i not in self.cn.tabs():
+                to_be_deleted.append(i)
+
+        for i in to_be_deleted:
+            del self.tabs[i]
+
+        # tab_names =  self.cn.tabs()
+        # print("tabnames: ", tab_names)
+        # tn = [ i for i in self.tabs.keys()]
+        # print("tabnames2: ", tn)
+        # print("current: " , self.cn.select())
+
+        if filename in [self.tabs[j][1] for j in self.tabs.keys()]:
+            return
+
         if filename != '' and len(filename)>0:
             with open(filename, "r+") as f:
-                if self.scrolled_text == None:
-                    self.scrolled_text = ScrolledText(self, width=750)
-                    self.scrolled_text.pack(side="left", fill="both", expand=True)
-                self.scrolled_text.delete('1.0',END)
+                # frame = tk.Frame(notebook, background=color)
+                st = ScrolledText(self, width=750)
                 prog = f.read()
-                self.scrolled_text.insert("1.0", prog )
-                FILE_OPEN = 1
-                root.title((filename + " -- Simulator"))
+                st.insert("1.0", prog )
+                c = filename.split('/')
+                c = c[len(c)-1]
+                self.cn.add(st, text= c)
+                self.cn.select(self.cn.index("end")-1)
+                self.tabs[self.cn.select()]=(st,filename)
+                print(self.cn.index("end"))
+
+                # scrolled_text.pack(side="left", fill="both", expand=True)
+
+                # if self.scrolled_text == None:
+                #     self.scrolled_text = ScrolledText(self, width=750)
+                #     self.scrolled_text.pack(side="left", fill="both", expand=True)
+                # self.scrolled_text.delete('1.0',END)
+                # root.title((filename + " -- Simulator"))
                 runmenu.entryconfig(1,state=NORMAL)
                 runmenu.entryconfig(2,state=NORMAL)
 
@@ -76,57 +119,142 @@ class Code(Frame):
 
         # with open(filename, "r") as f:
         #     self.scrolled_text.insert("1.0", f.read())
-
+        tab_names = [ self.cn.tabs()]
+        print("tabnames: ", tab_names)
+        tn = [ i for i in self.tabs.keys()]
+        print("tabnames2: ", tn)
+        print("current: " , self.cn.select())
 
 
 
 
     def onNew(self):
         global root
-        global FILE_OPEN
-        if FILE_OPEN == 0:
-            self.current_file = None
-            self.scrolled_text = ScrolledText(self)
-            self.scrolled_text.pack(side="top", fill="both", expand=True)
-            self.scrolled_text.delete('1.0',END)
-            FILE_OPEN=1
+        self.filecount = self.filecount+1
+        self.current_file = "Untitled" + str(self.filecount)
+        st = ScrolledText(self, width=200)
+        st.pack(side = "left",fill='both')
+        self.cn.add(st, text=self.current_file)
+        self.cn.select(self.cn.index("end")-1)
 
-        else:
-            if messagebox.askyesno("Simulator","Do you want to save the changes?"):
-                self.onSave()
-            self.scrolled_text.delete('1.0',END)
+        self.tabs[self.cn.select()]=(st,self.current_file)
+        print(self.cn.index("end"))
+
+
+
+        # self.scrolled_text = ScrolledText(self)
+        # self.scrolled_text.pack(side="top", fill="both", expand=True)
+        # self.scrolled_text.delete('1.0',END)
+
+        # if messagebox.askyesno("Simulator","Do you want to save the changes?"):
+        #     self.onSave()
+        # self.scrolled_text.delete('1.0',END)
 
 
 
 
     def onSave(self):
+        global runmenu
         f = None
-        if self.current_file == None:
-            ftypes = [('Assembly files', '*.asm'), ('All files', '*')]
+        self.current_file = self.cn.tab(self.cn.select(), "text")
+        if 'Untitled' == self.current_file[:8] :
+            ftypes = [('Assembly files', '*.asm')]
             f = filedialog.asksaveasfile(initialdir = "~",mode='w', defaultextension=".asm", filetypes = ftypes)
+            self.tabs[self.cn.select()][1] = f.name
             # print("filename: " , self.current_file)
             if f is None: # asksaveasfile return `None` if dialog closed with "cancel".
                 return
-            self.current_file = f.name
+            # self.current_file = f.name
 
         else:
-            f = open(self.current_file,"w")
+            file = self.tabs[self.cn.select()][1]
+            print("file: ",file)
+            f = open(file,"w")
 
-        text2save = str(self.scrolled_text.get(1.0, END)) # starts from `1.0`, not `0.0`
+
+        runmenu.entryconfig(1,state=NORMAL)
+        runmenu.entryconfig(2,state=NORMAL)
+        text2save = str(self.tabs[self.cn.select()][0].get(1.0, END)) # starts from `1.0`, not `0.0`
         f.write(text2save)
         f.close()
 
 
 
 
-    def onRun(self):
-        if self.scrolled_text.edit_modified():
-            self.onSave()
 
 
 
-    def onAssem():
+
+    def onRunAll(self):
+        # self.cn.forget(self.cn.index(self.cn.select()))
+        # self.cn.event_generate("<<NotebookTabClosed>>")
+        # tab_names =  self.cn.tabs()
+        # print("\n\n[run]\ntabnames: ", tab_names)
+        # tn = [ i for i in self.tabs.keys()]
+        # print("tabnames2: ", tn)
+        # print("current: " , self.cn.select())
+        for i in self.tabs:
+            self.cn.forget(self.cn.index(i))
+        e = ExecutionGUI(self)
+        e.pack(side = 'left',fill ="both")
+        self.cn.add(e, text="exec")
+        self.cn.select(self.cn.index("end")-1)
+
+
+    def onRunStepByStep():
         pass
+
+
+
+    def onAssemCurrent(self):
+        global console
+        # print("Inside assemble current")
+        filename = self.tabs[self.cn.select()][1]
+        print("\n Filename", filename)
+        console.config(state=NORMAL)
+
+
+        cons = str(console.get(1.0, END))
+        if len(cons) > 700:
+            cons = cons[200:]
+            console.delete('1.0',END)
+            console.insert('1.0', cons)
+
+        if assembler.assemble(filename,0) ==1:
+            console.insert(END,filename+">"+assembler.error)
+        else:
+            console.insert(END,"\n\n"+ filename+">"+" DONE")
+
+        console.config(state=DISABLED)
+
+
+    def onAssemAll(self):
+        global console
+        global assembler_output
+        assembler_output= dict()
+        cons = str(console.get(1.0, END))
+        console.config(state=NORMAL)
+        if len(cons) > 700:
+            cons = cons[400:]
+            console.delete('1.0',END)
+            console.insert('1.0', cons)
+
+        print("tabs: ",self.tabs)
+        end=0
+        for _,filename in self.tabs.values():
+            beg = end
+            if assembler.assemble(filename,beg) ==1:
+                console.insert(END,filename+">"+assembler.error)
+
+            else:
+                console.insert(END,"\n\n"+ filename+">"+" DONE")
+                end = assembler.binary_file_linecount
+                assembler_output[filename]=assembler.equivalent_binary
+                # print("\n\n\n\n in main_gui\n\n ", assembler.equivalent_binary)
+
+        console.config(state=DISABLED)
+        print("assembler outputs in main gui\n\n\n",assembler_output)
+
 
 
 
@@ -135,27 +263,34 @@ def add_top_menu(frame):
     global ASSEMBLING
     global FILE_OPEN
     global runmenu
+    global c
     menu = Menu(frame, bg = '#20b2aa')
-    frame.config(menu=menu)
+
     filemenu = Menu(menu)
     menu.add_cascade(label='File', menu=filemenu)
     runmenu = Menu(menu)
     menu.add_cascade(label = 'Run', menu=runmenu)
-    filemenu.add_command(label="Open" )
-    filemenu.add_command(label="New" )
-    filemenu.add_command(label="Save")
+
+    filemenu.add_command(label="Open", command=c.onOpen)
+    filemenu.add_command(label="New", command=c.onNew)
+    filemenu.add_command(label="Save", command=c.onSave)
     filemenu.add_separator()
     filemenu.add_command(label='Exit', command=frame.quit)
 
-    runmenu.add_cascade(label = 'Compile/Assemble current')
-    runmenu.add_cascade(label = 'Compile/Assemble all')
-    runmenu.add_cascade(label = 'Run all')
-    runmenu.add_cascade(label= 'Run Step by step')
+
+
+    runmenu.add_command(label = 'Compile/Assemble Current', command= c.onAssemCurrent)
+    runmenu.add_command(label = 'Compile/Assemble All', command = c.onAssemAll )
+    runmenu.add_command(label = 'Run all', command=c.onRunAll)
+    runmenu.add_command(label= 'Run Step by step',command = c.onRunStepByStep)
+
     # runmenu.add_cascade(label= 'Run Next Step')
-    runmenu.entryconfig(1,state=DISABLED)
-    runmenu.entryconfig(2,state=DISABLED)
-    runmenu.entryconfig(3,state=DISABLED)
-    runmenu.entryconfig(4,state=DISABLED)
+    # print(c.cn.index("end"))
+    # if c.cn.index("end") ==0:
+    #     runmenu.entryconfig(1,state=DISABLED)
+    #     runmenu.entryconfig(2,state=DISABLED)
+    #     runmenu.entryconfig(3,state=DISABLED)
+    #     runmenu.entryconfig(4,state=DISABLED)
 
 
 
@@ -164,10 +299,17 @@ def add_top_menu(frame):
     menu.add_cascade(label='Help', menu=helpmenu)
     helpmenu.add_command(label='About')
 
+    frame.config(menu=menu)
 
 
 
+def disable_frame(f):
+    for child in f.winfo_children():
+        child.configure(state='disable')
 
+def enable_frame(f):
+    for child in frame2.winfo_children():
+        child.configure(state=NORMAL)
 
 
 def update_registers(T):
@@ -178,54 +320,63 @@ def update_registers(T):
                  'ANS':'1011', 'IR': '1100', 'IHAR':'1101' }
 
     str_val = ''
+
     for i in REGISTERS:
-        str_val = '\n\n\n' + i + " : " + REGISTERS[i]
+        str_val = '\n\n\n  ' + i + " : " + REGISTERS[i]
         T.insert(END, str_val)
 
     T.config(state=DISABLED)
-
-    # print("str_val=", str_val)
-
-
-
-
 
 
 
 
 if __name__ == '__main__':
     global root
+    global console
+    global c
     # creating main window object
     root = Tk()
-    root.geometry("830x650")
+    root.geometry("1400x700")
     root.title('Simulator')
-    registers = Frame(root, width = 30)
+
+    registers = Frame(root, width = 30,highlightbackground="black", highlightcolor="black", highlightthickness=2)
     pin_console_code = Frame(root, bg = 'gray95', width = 800)
-    pins = Frame(pin_console_code, width =30, bg = 'gray80')
 
-    console = ScrolledText(pin_console_code,bg='#bfd9d9', height = 14)
-    console.config(state=DISABLED)
 
-    notebook = CustomNotebook(pin_console_code,width=200, height=200)
 
 
     registers.pack(side = "left",fill="both")
     pin_console_code.pack(side = "right",fill="both", expand = True)
 
 
+    pins = Frame(pin_console_code, width =30, bg = 'gray85')
+    p = Frame(pins, width =30, bg = 'gray80', height = 50,highlightbackground="black", highlightcolor="black", highlightthickness=1)
+    c= Code(pin_console_code,bg = 'gray89', width=700, height =100) # inserting CODE GUI
+    console = ScrolledText(pin_console_code,bg='#bfd9d9', height = 14,width=800)
+    console.config(state=DISABLED)
 
-    # inserting CODE GUI in right frame
-    # c= Code(pin_console_code)
+
     console.pack(side='bottom', fill='both')
-    notebook.pack(side="left", fill="both", expand=True)
-    pins.pack(side = "left",fill='both')
-    # c.pack(side="left", fill="both" ,expand=True)
-
+    c.pack(side="left", fill="both")
+    pins.pack(side = "right",fill='both')
+    p.pack(side = "bottom", pady=20)
 
     # adding pins
     var = IntVar()
-    ch = Checkbutton(pins, text="Interrupt", variable=var, width = 30, bg='gray80')
-    ch.pack(side = "left",fill='both')
+    ch = Checkbutton(p, text="  Interrupt", variable=var, width = 20, bg='gray80')
+    description = Label(p, text = "Interrupt and Data\n Communication pins ", width =15)
+    note = Label(p, text = "NOTE: give interrupt data as \n4 character hexadecimal value with\n no prefixes (such as # or H)", width =15)
+    L1 = Label(p, text = "Interrupt Data", width =15)
+    button = Button(p, text="send")
+    E1 = Entry(p, bd = 5, width=14 )
+
+    description.pack(side = "top", pady = 10, fill='x')
+    ch.pack(side = "top", pady = 10)
+    note.pack(side = "bottom", fill='x')
+    button.pack(side=BOTTOM, pady = 10)
+    L1.pack( side = LEFT, pady = 10)
+    E1.pack(side = RIGHT, fill = 'x' , pady = 10)
+    # disable_frame(p)
 
 
     #creating top menu
@@ -233,6 +384,7 @@ if __name__ == '__main__':
 
 
     # inserting values in register frame
+    # reg = Label(registers)
     T = Text(registers, width = 30, bg ='gray80')
     T.pack(side = 'top', fill='both',expand = True)
     T.config(state=DISABLED)
@@ -240,18 +392,3 @@ if __name__ == '__main__':
 
 
     mainloop()
-
-
-# import customNotebook as cn
-# import tkinter as tk
-# if __name__ == "__main__":
-#     root = tk.Tk()
-#
-#     notebook = cn.CustomNotebook(width=200, height=200)
-#     notebook.pack(side="top", fill="both", expand=True)
-#
-#     for color in ("red", "orange", "green", "blue", "violet"):
-#         frame = tk.Frame(notebook, background=color)
-#         notebook.add(frame, text=color)
-#
-#     root.mainloop()
