f42a1944767e2e97ce86e2a7fb2680075e333cae
Ruchi Saha <ruchi@Ruchi.localdomain>
2019-10-01 11:59:57 +0530

    minor changes

--- Simulator/Execution.py
+++ Simulator/Execution.py
@@ -2,7 +2,7 @@
 
 
 
-import tkinter as tk
+from tkinter import *
 
 
 OP_CODES={ 'COMP':'0000', 'MOV':'0001', 'INC':'0010',
@@ -30,37 +30,61 @@ UNARY_OPS = ['INC', 'JUMP','FETCH','JUMPEQ', 'DEC']
 
 
 
-class ExecutionGUI(tk.Frame):
+class ExecutionGUI(Frame):
     def __init__(self, root):
 
-        tk.Frame.__init__(self, root)
-        self.canvas = tk.Canvas(root, borderwidth=0, background="#ffffff")
-        self.frame = tk.Frame(self.canvas, background="#ffffff")
-        self.vsb = tk.Scrollbar(root, orient="vertical", command=self.canvas.yview)
+        Frame.__init__(self, root)
+        self.canvas = Canvas(root, borderwidth=0, background="#ffffff")
+        self.frame = Frame(self.canvas, background="#ffffff")
+        self.vsb = Scrollbar(self, orient="vertical", command=self.canvas.yview)
         self.canvas.configure(yscrollcommand=self.vsb.set)
 
         self.vsb.pack(side="right", fill="y")
-        self.canvas.pack(side="left", fill="both", expand=True)
+        self.canvas.pack(side="left", fill="both",expand=True)
         self.canvas.create_window((4,4), window=self.frame, anchor="nw",
                                   tags="self.frame")
 
         self.frame.bind("<Configure>", self.onFrameConfigure)
+
+        # self.canvas = tk.Canvas(root, borderwidth=0, background="#ffffff")
+        # self.frame = tk.Frame(self.canvas, background="#ffffff")
+        # self.vsb = tk.Scrollbar(root, orient="vertical", command=self.canvas.yview)
+        # self.canvas.configure(yscrollcommand=self.vsb.set)
+        #
+        # self.vsb.pack(side="right", fill="y")
+        # self.canvas.pack(side="left", fill="both", expand=True)
+        # self.canvas.create_window((4,4), window=self.frame, anchor="nw",
+        #                           tags="self.frame")
+
+        # self.frame.bind("<Configure>", self.onFrameConfigure)
 
         self.populate()
 
     def populate(self):
         '''Put in some fake data'''
         for row in range(100):
-            tk.Label(self.frame, text="%s" % row, width=3, borderwidth="1",
+            Label(self.frame, text="%s" % row, width=7, borderwidth="1",
                      relief="solid").grid(row=row, column=0)
             t="this is the second column for row %s" %row
-            tk.Label(self.frame, text=t).grid(row=row, column=1)
+            Label(self.frame, text=t).grid(row=row, column=1)
+
+    def populate_code(self,b):
+        pass
+
+
 
     def onFrameConfigure(self, event):
         '''Reset the scroll region to encompass the inner frame'''
         self.canvas.configure(scrollregion=self.canvas.bbox("all"))
 
+
+
+
+
+
 if __name__ == "__main__":
-    root=tk.Tk()
-    Example(root).pack(side="top", fill="both", expand=True)
+    root=Tk()
+    t=Frame(root)
+    # t.pack(side=TOP)
+    ExecutionGUI(root).pack(side="top", fill="both", expand=True)
     root.mainloop()
